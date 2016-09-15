dd16c56dee67130d025196aa5ae77981289c9311
Damien Storm <damien.r.storm@gmail.com>
2016-09-15 10:17:02 -0400

    added favicon & removed obsolete code and comments

--- public/js/app.js
+++ public/js/app.js
@@ -181,28 +181,15 @@ function getSoundFile() {
 }
 
 function playDiceSound() {
-  //Create the audio tag
   var soundFile = document.createElement("audio");
   soundFile.preload = "auto";
-
-  //Load the sound file (using a source element for expandability)
   var src = document.createElement("source");
   var soundLocation = getSoundFile();
   src.src = getSoundFile();
   soundFile.appendChild(src);
-
-  //Load the audio tag
-  //It auto plays as a fallback
   soundFile.load();
   soundFile.volume = 0.9;
-  soundFile.play();
-
-  //Set the current time for the audio file to the beginning
-  // soundFile.currentTime = 0.01;
-  // soundFile.volume = volume;
-
-  //Due to a bug in Firefox, the audio needs to be played after a delay
-  // setTimeout(function(){soundFile.play();},1);
+  setTimeout(function(){soundFile.play();},1);
 }
 
 jQuery(function($) {
