c06fce06d036fae1bb6395c5c070940f16c349a3
Damien <damien.r.storm@gmail.com>
2016-10-09 14:42:55 -0400

    moved files out of the public & views folder and added index.html file so the repo will work with github pages and be simpler to work with

--- thornwatch_dice_roller.rb
+++ thornwatch_dice_roller.rb
@@ -2,8 +2,12 @@ require 'awesome_print'
 require 'sinatra'
 require 'pry'
 
+set :root, File.dirname(__FILE__)
+set :views, File.dirname(__FILE__)
+set :public_folder, File.dirname(__FILE__)
 set :bind, '0.0.0.0'
 set :port, 5555
+
 get '/' do
-  haml :die_select
+  haml :index
 end
